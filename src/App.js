ce0311e2537e1886d08ef627fe28bcc0540af0b5
WerriesS <werries.s@gmail.com>
2018-08-03 04:39:21 +0200

    Attempt to display conversion result

--- src/App.js
+++ src/App.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const ApiEndpoint = "https://free.currencyconverterapi.com/api/v6/";
+const ConvertEndpoint = ApiEndpoint + "convert?q=";
+const CompactEndpoint = "&compact=y";
+
+class Heading extends Component {
+  constructor(props) {
+    super(props);
+    let request = "USD_ZAR";
+    this.state = this.getConversionResult(request);
+  }
+  getConversionResult(request) {
+    const conversionRequest = ConvertEndpoint + request + CompactEndpoint;
+    return fetch(conversionRequest)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let conversionResult = data[request];
+        return conversionResult;
+      })
+  }
+
+  render() {
+    let request = "USD_ZAR";
+    return (
+      <div>
+        {request}: {this.state}
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +45,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Heading />
       </div>
     );
   }

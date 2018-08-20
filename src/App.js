c54b6430ee0cbc7356a49a6b68fa07f892c3f34e
WerriesS <werries.s@gmail.com>
2018-08-20 20:20:20 +0200

    Add local storage to prevent fetching of currencies unnecessarily and update styling

--- src/App.js
+++ src/App.js
@@ -12,14 +12,14 @@ var currencies = null;
 class CurrencyConversion extends Component {
   constructor(props) {
     super(props);
-    this.state = {conversionDisplay: 'Please select currencies to convert.'};
+    this.state = { conversionDisplay: 'Please select currencies to convert.' };
   }
 
   async componentDidUpdate(prevProps) {
     if (this.props.fromRequest !== null
       & this.props.toRequest !== null
       & (this.props.fromRequest !== prevProps.fromRequest
-      | this.props.toRequest !== prevProps.toRequest)
+        | this.props.toRequest !== prevProps.toRequest)
       & this.props.toRequest !== this.props.fromRequest) {
       let request = this.props.fromRequest.value + '_' + this.props.toRequest.value;
       let conversionResult = await this.getConversionResult(request);
@@ -56,13 +56,13 @@ class CurrencyConversion extends Component {
 
   render() {
     return (
-        <Grid className={'currency-conversion'}>
-          <Row>
-            <Col md={12}>
-              {this.state.conversionDisplay}
-            </Col>
-          </Row>
-        </Grid>
+      <Grid className={'currency-conversion'}>
+        <Row>
+          <Col md={12}>
+            {this.state.conversionDisplay}
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
@@ -79,43 +79,61 @@ class CurrencySelector extends Component {
   }
 
   async getCurrencies() {
-    let response = await fetch(CurrenciesEndpoint);
-    let data = await response.json();
-    let currenciesResult = data['results'];
-    currencies = currenciesResult;
+    if (LocalStorageIsActive()) {
+      if (localStorage.getItem('Currencies')) {
+        currencies = JSON.parse(localStorage.getItem('Currencies'));
+      } else {
+        alert('Fetching Currencies for local storage.');
+        let response = await fetch(CurrenciesEndpoint);
+        let data = await response.json();
+        let currenciesResult = data['results'];
+        localStorage.setItem('Currencies', JSON.stringify(currenciesResult));
+        currencies = JSON.parse(localStorage.getItem('Currencies'));
+      }
+    } else {
+      if (currencies === null || !currencies) {
+        alert('Fetching Currencies without local storage.');
+        let response = await fetch(CurrenciesEndpoint);
+        let data = await response.json();
+        let currenciesResult = data['results'];
+        currencies = currenciesResult;
+      }
+    }
 
-    let optionsArray = Object.keys(currenciesResult).map((key) => {
-      let currencyEntry = { value: currenciesResult[key].id, label: currenciesResult[key].currencyName };
+    let optionsArray = Object.keys(currencies).map((key) => {
+      let currencyEntry = { value: currencies[key].id, label: currencies[key].currencyName };
       return currencyEntry;
     });
 
     optionsArray.sort((a, b) => {
-      if ( a.label < b.label ) return -1;
-      if ( a.label > b.label ) return 1;
+      if (a.label < b.label) return -1;
+      if (a.label > b.label) return 1;
       return 0;
     });
 
-    this.setState({options: optionsArray}) ;
+    this.setState({ options: optionsArray });
   }
 
   handleFromChange = (selectedFromOption) => {
-    this.setState({selectedFromOption});
+    this.setState({ selectedFromOption });
   }
   handleToChange = (selectedToOption) => {
-    this.setState({selectedToOption});
+    this.setState({ selectedToOption });
   }
 
   render() {
-    const {selectedFromOption, selectedToOption} = this.state;
+    const { selectedFromOption, selectedToOption } = this.state;
 
     return (
       <div>
         <Grid>
           <Row>
-            <Col md={6}>
+            <Col md={6} className={'select-element'}>
               <Row>
                 <Col md={12}>
-                  Select from currency
+                  <p>
+                    Select from currency
+                  </p>
                 </Col>
               </Row>
               <Row>
@@ -127,10 +145,12 @@ class CurrencySelector extends Component {
                 </Col>
               </Row>
             </Col>
-            <Col md={6}>
+            <Col md={6} className={'select-element'}>
               <Row>
                 <Col md={12}>
-                  Select to currency
+                  <p>
+                    Select to currency
+                  </p>
                 </Col>
               </Row>
               <Row>
@@ -169,5 +189,22 @@ class App extends Component {
   }
 }
 
+function LocalStorageIsActive() {
+  if (typeof localStorage !== 'undefined') {
+    try {
+      localStorage.setItem('feature_test', 'yes');
+      if (localStorage.getItem('feature_test') === 'yes') {
+        localStorage.removeItem('feature_test');
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
 
 export default App;
